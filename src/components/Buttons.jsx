42b979a7ac2f8907fac9c0e7c3880a8ef2d30049
Faisal Ahmed <faisalahmed8988@gmail.com>
2023-09-16 23:24:49 +0600

    all functionality completed

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -10,6 +10,10 @@ export const MultiButton = () => {
 
   const [showRow, setshowRow] = useState(false);
   const [roows, setRows] = useState([]); // State to store saved titles
+  const [editedRowName, setEditedRowName] = useState("");
+  const [editedRowLink, setEditedRowLink] = useState("");
+  const [editedRowIsDone, setEditedRowIsDone] = useState(false);
+  const [editedRowNeedRevise, setEditedNeedRevise] = useState(false);
 
   const handleSaveRow = (newRow) => {
     setRows([...roows, newRow]);
@@ -32,10 +36,19 @@ export const MultiButton = () => {
     setTitles(updatedTitles);
   };
 
+  const handleRowsClick = (index) => {
+    // Toggle the selected state of the clicked title
+    const updatedRows = [...roows];
+    updatedRows[index].isSelected = !updatedRows[index].isSelected;
+    setRows(updatedRows);
+  };
+
   const handleDeleteSelected = () => {
     // Filter out the selected titles and update the state
     const updatedTitles = titles.filter((title) => !title.isSelected);
+    const updatedRows = roows.filter((row) => !row.isSelected);
     setTitles(updatedTitles);
+    setRows(updatedRows);
   };
 
   const handleTitleDoubleClick = (index) => {
@@ -45,12 +58,46 @@ export const MultiButton = () => {
     setTitles(updatedTitles);
   };
 
+  const handleRowDoubleClick = (index) => {
+    // Enable editing mode for the double-clicked title
+    const updatedRows = [...roows];
+    updatedRows[index].isEditing = true;
+    setRows(updatedRows);
+  };
+
   const handleEditTitleChange = (index, updatedTitle) => {
-    // Update the edited title
     const updatedTitles = [...titles];
     updatedTitles[index].title = updatedTitle;
     updatedTitles[index].isEditing = false; // Disable editing mode
     setTitles(updatedTitles);
+  };
+
+  const handleEditRowNameChange = (index, updatedRowName) => {
+    const updatedRows = [...roows];
+    updatedRows[index].name = updatedRowName;
+    updatedRows[index].isEditing = false; // Disable editing mode
+    setRows(updatedRows);
+  };
+
+  const handleRowLinkChange = (index, updatedRowLink) => {
+    const updatedRows = [...roows];
+    updatedRows[index].link = updatedRowLink;
+    updatedRows[index].isEditing = false; // Disable editing mode
+    setRows(updatedRows);
+  };
+
+  const handleIsDoneChange = (index) => {
+    const updatedRows = [...roows];
+    updatedRows[index].isDone = !updatedRows[index].isDone;
+    updatedRows[index].isEditing = false; // Disable editing mode
+    setRows(updatedRows);
+  };
+
+  const handleNeedsRevisionChange = (index) => {
+    const updatedRows = [...roows];
+    updatedRows[index].needsRevision = !updatedRows[index].needsRevision;
+    updatedRows[index].isEditing = false; // Disable editing mode
+    setRows(updatedRows);
   };
 
   return (
@@ -103,7 +150,6 @@ export const MultiButton = () => {
                       onChange={() => handleTitleClick(index)}
                     />
                     {title.title}
-                    {roows}
                   </>
                 )}
               </li>
@@ -113,14 +159,69 @@ export const MultiButton = () => {
           <div className="rowContainer">
             <ul className="rowBox">
               {roows.map((row, index) => (
-                <li key={index}>
-                  <input type="checkbox" />
-                  <span>Name: {row.name}</span>
-                  <span>Link: {row.link}</span>
-                  <span>Done: {row.isDone ? "Yes" : "No"}</span>
-                  <span>
-                    Needs Revision: {row.needsRevision ? "Yes" : "No"}
-                  </span>
+                <li
+                  key={index}
+                  onDoubleClick={() => handleRowDoubleClick(index)} // Enable editing on double-click
+                >
+                  {row.isEditing ? ( // Check if the row is in edit mode
+                    <div className="rowInfo">
+                      <input
+                        type="checkbox"
+                        checked={row.isSelected}
+                        onChange={() => handleRowsClick(index)}
+                      />
+                      <input
+                        type="text"
+                        value={editedRowName}
+                        onChange={(e) => setEditedRowName(e.target.value)}
+                        onKeyDown={(e) => {
+                          if (e.key === "Enter") {
+                            handleEditRowNameChange(index, editedRowName);
+                          }
+                        }}
+                      />
+                      <input
+                        type="text"
+                        value={editedRowLink}
+                        onChange={(e) => setEditedRowLink(e.target.value)}
+                        onKeyDown={(e) => {
+                          if (e.key === "Enter") {
+                            handleRowLinkChange(index, editedRowLink);
+                          }
+                        }}
+                      />
+                      <div>
+                        Done:{" "}
+                        <input
+                          type="checkbox"
+                          checked={row.isDone}
+                          onChange={() => handleIsDoneChange(index)}
+                        />
+                      </div>
+                      <div>
+                        Needs Revision:{" "}
+                        <input
+                          type="checkbox"
+                          checked={row.needsRevision}
+                          onChange={() => handleNeedsRevisionChange(index)}
+                        />
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="rowInfo">
+                      <input
+                        type="checkbox"
+                        checked={row.isSelected}
+                        onChange={() => handleRowsClick(index)}
+                      />
+                      <span>Name: {row.name}</span>
+                      <span>Link: {row.link}</span>
+                      <span>Done: {row.isDone ? "Yes" : "No"}</span>
+                      <span>
+                        Needs Revision: {row.needsRevision ? "Yes" : "No"}
+                      </span>
+                    </div>
+                  )}
                 </li>
               ))}
             </ul>
